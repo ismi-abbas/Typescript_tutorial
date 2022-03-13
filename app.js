abf5f0429281f8f826a38715d49727f95c6005eb
Abbas <muhdabbas98@gmail.com>
2022-03-13 20:10:57 +0800

    Revert "First commit"
    This reverts commit 7a69b917c141fa83b664fa03fd1703d43bff2c73.

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require("express");
-const app = express();
-// Start a server
-const port = process.env.PORT || 5000;
-app.listen(port, (req, res) => {
-    res.send(`Server started on port ${port}`);
-});
--- app.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-// Start a server
-const port = process.env.PORT || 5000;
-
-app.listen(port, (req: any, res: any) => {
-	res.send(`Server started on port ${port}`);
-});
