38bcde864634d2892e8a015fb9950fbca82bf08f
Abbas <muhdabbas98@gmail.com>
2022-06-24 00:01:10 +0800

    Render with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
 
   constructor() {
-    this.templateElement = document.getElementById('project-input');
-    this.hostElement = document.getElementById('')
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById('project-input')!
+    );
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
+const projectjInput = new ProjectInput();
